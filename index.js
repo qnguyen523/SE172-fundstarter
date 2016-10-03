ed9949d6f99c4d8d9c72dd6e00b0232ec5506569
Peter <qnguyen523@gmail.com>
2016-10-03 15:21:50 -0700

    deploy Part 1 a

--- index.js
+++ index.js
@@ -2,12 +2,10 @@
 // Homework 2
 // Class: SE 172 
 
-// This is Part 2
-
+// This is Part 1 a)
 var express = require('express')
 var app = express()
 var fs = require('fs');
-var buf = new Buffer(10240);
 
 app.set('port', (process.env.PORT || 8080))
 //app.use(express.static(__dirname + '/public'))
@@ -17,34 +15,13 @@ app.set('port', (process.env.PORT || 8080))
 app.get('/', function(request, response)
 {
 	// handle send file here.
-	fs.open('public/index.html', 'r+', function(err, fd)
-	{
-		if(err)
-		{
-			console.log(err);
-		}
-		fs.read(fd, buf, 0, buf.length, 0, function(err, bytes)
-		{
-			if (err)
-			{
-		    	console.log(err);
-		    }
-		    // response only read byte on the index.html file
-		    if(bytes > 0)
-		    {
-		    	response.send(buf.slice(0, bytes).toString())
-      		}
-      		// Close the index.html file.
-	      fs.close(fd, function(err)
-	      {
-	         if (err)
-	         {
-	            console.log(err);
-	         } 
-	         console.log("File closed successfully.");
-	      });
-		});
-	});
+	var data = fs.readFileSync('public/index.html');
+	response.send(data.toString());
+    // response.sendFile('public/index.html',{root:__dirname})
+
+ // sends an entire HTTP response to the client,                                                                                                                                     
+ // including headers and content,                                                                                                                                                     
+ // which is why you can only call once
 
 
 })
