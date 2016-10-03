a8447bf42cf589c85a86c1495f3a435c63f60b3a
Peter <qnguyen523@gmail.com>
2016-10-03 15:08:21 -0700

    Part 1 a

---
+++ index.js
@@ -0,0 +1,32 @@
+// My name: Quoc Nguyen
+// Homework 2
+// Class: SE 172 
+
+// This is Part 1 a)
+var express = require('express')
+var app = express()
+var fs = require('fs');
+
+app.set('port', (process.env.PORT || 8080))
+//app.use(express.static(__dirname + '/public'))
+
+//__dirname returns the directory that the currently executing script is in.
+
+app.get('/', function(request, response)
+{
+	// handle send file here.
+	var data = fs.readFileSync('public/index.html');
+	response.send(data.toString());
+    // response.sendFile('public/index.html',{root:__dirname})
+
+ // sends an entire HTTP response to the client,                                                                                                                                     
+ // including headers and content,                                                                                                                                                     
+ // which is why you can only call once
+
+
+})
+
+app.listen(app.get('port'), function()
+{
+  console.log("Node app is running at :" + app.get('port'))
+});
