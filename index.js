82db746a12d0d4521a3f7dcc2adf8e9722bcb018
Peter <qnguyen523@gmail.com>
2016-10-06 15:27:51 -0700

    deploy app on heroku

--- index.js
+++ index.js
@@ -4,17 +4,36 @@
 
 // This is Part 1 a)
 
+var http = require('http');
+var port = Number(process.env.PORT || 8080);
 var fs = require('fs');
-var http = require("http");
-http.createServer(function (request, response)
-{
-	// handle send file here.
-	fs.readFile('public/index.html',function(err, data)
-	{
-		if (err) throw err;
-		response.write(data.toString());
-		response.end();
-	});
-}).listen(8080);
+var buf = new Buffer(10240);
 
-console.log('Node app is running at port:8080');
+var requestListener = function(req, res){
+	//check if file exists
+	fs.exists('public/index.html', function(exists) {
+	if(!exists) {
+		res.writeHead(404, {"Content-Type": "text/html"});
+		res.write("404 Not Found\n");
+		res.end();
+		return;
+	}});
+
+	fs.readFile('public/index.html', function (err, data) {
+   if (err) {
+		 res.writeHead(400, {"Content-Type": "text/html"});
+		 res.write("Error\n");
+		 res.end();
+      return;
+   }
+	 res.writeHead(200, {"Content-type":"text/html"});
+
+    res.end(data);
+});
+
+}
+
+var server = http.createServer(requestListener);
+server.listen(port,function(){
+	console.log('Node app is running at port:',port);
+});
