ba65f3a4fa1e3b346a05a87b801d0311d1a4c7e4
Peter <qnguyen523@gmail.com>
2016-10-06 15:46:32 -0700

    deploy part 2

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ var port = Number(process.env.PORT || 8080);
 var fs = require('fs');
 var buf = new Buffer(10240);
 
-var requestListener = function(req, res){
-	//check if file exists
+var requestListener = function(req, res)
+{
+	// check if the index.html file exists
 	fs.exists('public/index.html', function(exists) {
 	if(!exists) {
 		res.writeHead(404, {"Content-Type": "text/html"});
@@ -18,11 +19,41 @@ var requestListener = function(req, res){
 		res.end();
 		return;
 	}});
+	// handle send file here.
+	fs.open('public/index.html', 'r+', function(err, fd)
+	{
+		if(err)
+		{
+			console.log(err);
+		}
+		fs.read(fd, buf, 0, buf.length, 0, function(err, bytes)
+		{
+			if (err)
+			{
+		    	console.log(err);
+		    }
+		    // response only read byte on the index.html file
+		    if(bytes > 0)
+		    {
+		    	res.writeHead(200, {"Content-type":"text/html"});
+ 				res.end(buf.slice(0, bytes).toString());
+      		}
+      		// Close the index.html file.
+	      fs.close(fd, function(err)
+	      {
+	         if (err)
+	         {
+	            console.log(err);
+	         } 
+	         console.log("File closed successfully.");
+	      });
+		});
+	});
 
 
-	var data = fs.readFileSync('public/index.html');
-	res.writeHead(200, {"Content-type":"text/html"});
-    res.end(data);
+	// var data = fs.readFileSync('public/index.html');
+	// res.writeHead(200, {"Content-type":"text/html"});
+ //    res.end(data);
 };
 
 var server = http.createServer(requestListener);
