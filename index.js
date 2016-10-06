2635208f438313402a674812b7558a1ffca2afab
Peter <qnguyen523@gmail.com>
2016-10-06 13:56:44 -0700

    Part 1a with HTTP

--- index.js
+++ index.js
@@ -2,54 +2,15 @@
 // Homework 2
 // Class: SE 172 
 
-// This is Part 2
+// This is Part 1 a)
 
-var express = require('express')
-var app = express()
 var fs = require('fs');
-var buf = new Buffer(10240);
-
-app.set('port', (process.env.PORT || 8080))
-//app.use(express.static(__dirname + '/public'))
-
-//__dirname returns the directory that the currently executing script is in.
-
-app.get('/', function(request, response)
+var http = require("http");
+http.createServer(function (request, response)
 {
-	// handle send file here.
-	fs.open('public/index.html', 'r+', function(err, fd)
-	{
-		if(err)
-		{
-			console.log(err);
-		}
-		fs.read(fd, buf, 0, buf.length, 0, function(err, bytes)
-		{
-			if (err)
-			{
-		    	console.log(err);
-		    }
-		    // response only read byte on the index.html file
-		    if(bytes > 0)
-		    {
-		    	response.send(buf.slice(0, bytes).toString())
-      		}
-      		// Close the index.html file.
-	      fs.close(fd, function(err)
-	      {
-	         if (err)
-	         {
-	            console.log(err);
-	         } 
-	         console.log("File closed successfully.");
-	      });
-		});
-	});
+	var data = fs.readFileSync('public/index.html');
+	response.write(data.toString());
+	response.end();
+}).listen(8080);
 
-
-})
-
-app.listen(app.get('port'), function()
-{
-  console.log("Node app is running at :" + app.get('port'))
-});
+console.log('Node app is running at port:8080');
