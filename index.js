7f6e3477537540af181a7e47a1b017bbe55fdb75
Peter <qnguyen523@gmail.com>
2016-10-06 14:17:41 -0700

    deploy heroku

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 // Homework 2
 // Class: SE 172 
 
-// This is Part 1 b)
+// This is Part 1 a)
+
 var fs = require('fs');
 var http = require("http");
 http.createServer(function (request, response)
